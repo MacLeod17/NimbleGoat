8c60f32bc81da9ac987e8803ba8dee0ad71d00bf
yaakgold <yaakovgoldberg007@gmail.com>
2021-04-12 19:55:50 -0400

    Computer and Game skeleton

--- Models/Game.cs
+++ Models/Game.cs
@@ -4,7 +4,36 @@ using System.Text;
 
 namespace NimbleGoat.Models
 {
-    class Game
+    public class Game
     {
+        #region Singleton
+        private static Game _instance;
+
+        public Game Instance { get { return _instance; } }
+
+        public Game()
+        {
+            _instance = this;
+        }
+        #endregion
+
+        public Player playerTurn;
+        public Player[] players;
+        public int[] board;
+        
+        public bool SwapTurn()
+        {
+            return true;
+        }
+
+        public bool EndTurn()
+        {
+            return true;
+        }
+
+        public bool Reset()
+        {
+            return true;
+        }
     }
 }
