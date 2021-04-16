b62b72c3b06f528b1490809f845d3f4442389d1f
snajera531 <60483863+snajera531@users.noreply.github.com>
2021-04-16 16:17:57 -0600

    goat

---
+++ Pages/GOAT - Page.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NimbleGoat.Pages
+{
+    /// <summary>
+    /// Interaction logic for GOAT___Page.xaml
+    /// </summary>
+    public partial class GOAT___Page : Page
+    {
+        public GOAT___Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
